ec50aaa0bd90b2d77d0cc4de3ba29503fdf991a6
fg-dev-esc <luis.fernandez@escotel.com.mx>
2025-09-10 12:13:56 -0600

    any

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -5,10 +5,7 @@ import { ThemeProvider as NextThemesProvider } from "next-themes"
 
 interface ThemeProviderProps {
   children: React.ReactNode
-  attribute?: string
-  defaultTheme?: string
-  enableSystem?: boolean
-  disableTransitionOnChange?: boolean
+  [key: string]: any
 }
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
